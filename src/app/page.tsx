f07590dfa4659f8f5a720c206194e9a427c624d5
Maissa Mathlouthi <maissamth@gmail.com>
2026-01-12 15:20:13 +0100

    Add Tailwind directives to globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="bg-red-900 border-500 border-yellow-500 text-white p-10">
       <h1>Maissa Mathlouthi</h1>
       <p>Full Stack Developer & Cybersecurity Specialist</p>
     </div>
